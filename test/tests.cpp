bca2a6a8d64222012e93f1c9d6454c9e8ae3175b
kseniyaa3 <98958076+kseniyaa3@users.noreply.github.com>
2022-03-29 19:17:39 +0300

    tests changed

--- test/tests.cpp
+++ test/tests.cpp
@@ -2,3 +2,56 @@
 
 #include <gtest/gtest.h>
 #include "Automata.h"
+
+TEST(case1, test1) {
+	Automata automat = Automata();
+    automat.on();
+    automat.coin(10);
+    automat.choice(1);
+    automat.cook();
+    STATE state = automat.getState();
+    EXPECT_EQ(CHECK, state);
+}
+TEST(case2, test2) {
+    Automata automat = Automata();
+    automat.on();
+    automat.coin(60);
+    automat.choice(2);
+    automat.cook();
+    automat.cancel();
+    automat.finish();
+    STATE state = automat.getState();
+    EXPECT_EQ(WAIT, state);
+}
+TEST(case3, test3) {
+    Automata automat = Automata();
+    automat.on();
+    automat.coin(60);
+    automat.choice(2);
+    automat.cook();
+    automat.finish();
+    float cash = automat.off();
+    EXPECT_EQ(10.0, cash);
+}
+TEST(case4, test4) {
+    Automata automat = Automata();
+    automat.on();
+    automat.cook();
+    automat.choice(2);
+    STATE state = automat.getState();
+    EXPECT_EQ(WAIT, state);
+}
+TEST(case5, test5) {
+    Automata automat = Automata();
+    automat.on();
+    automat.coin(40);
+    automat.choice(2);
+    automat.cook();
+    automat.cancel();
+    automat.coin(40);
+    automat.choice(2);
+    automat.cook();
+    automat.finish();
+    float cash = automat.off();
+    EXPECT_EQ(30.0, cash);
+}
