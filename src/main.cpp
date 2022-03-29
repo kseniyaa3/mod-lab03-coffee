626b6e868c63473341998b2f31b22265c6399b23
kseniyaa3 <98958076+kseniyaa3@users.noreply.github.com>
2022-03-29 19:16:33 +0300

    main changed

--- src/main.cpp
+++ src/main.cpp
@@ -5,14 +5,10 @@ int main()
 {
     Automata automat;
     automat.on();
-    automat.coin(40);
-    automat.choice(2);
-    automat.cook();
-    automat.cancel();
-    automat.coin(40);
+    automat.coin(60);
     automat.choice(2);
     automat.cook();
     automat.finish();
-    float cash = automat.off();
+    automat.off();
     return 0;
 }
